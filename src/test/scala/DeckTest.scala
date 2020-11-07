518f7b625a1b44fd59bf6695654e2826c6860684
Kate Fulton <katefulton@richard.lan>
2020-11-06 19:58:51 -0800

    Test cleanup

--- src/test/scala/DeckTest.scala
+++ src/test/scala/DeckTest.scala
@@ -16,7 +16,7 @@ class DeckTest {
 
   @Test def deal(): Unit = {
     val deck: Deck = Deck.build
-    val (remainingDeck, hand) = Deck.deal(deck, 2).right.get
+    val Right(remainingDeck, hand) = Deck.deal(deck, 2)
     assertEquals(2, hand.cards.size)
     assertEquals(50, remainingDeck.cards.size)
   }
@@ -29,7 +29,7 @@ class DeckTest {
 
   @Test def dealHandEqualsDeck(): Unit = {
     val deck = Deck(List(Card(Ace, Heart), Card(Two, Heart)))
-    val (remainingDeck, hand) = Deck.deal(deck, 2).right.get
+    val Right(remainingDeck, hand) = Deck.deal(deck, 2)
     assertEquals(2, hand.cards.size)
     assertEquals(0, remainingDeck.cards.size)
   }
@@ -42,7 +42,7 @@ class DeckTest {
   
   @Test def pickCard(): Unit = {
     val deck = Deck(List(Card(Two, Heart)))
-    val (remainingDeck, card) = Deck.pickACardAnyCard(deck).right.get
+    val Right(remainingDeck, card) = Deck.pickACardAnyCard(deck)
     assertEquals(0, remainingDeck.cards.size)
     assertEquals(Card(Two, Heart), card)
   }
